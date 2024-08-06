66fbb4e76f231d530d72adf19965531d1e5098b6
NadiaPeki <napekarskaya@gmail.com>
2024-08-06 15:06:08 +0200

    created functions for generating embeddings

--- actions/generateEmbeddings.ts
+++ actions/generateEmbeddings.ts
@@ -4,8 +4,10 @@ import { revalidatePath } from 'next/cache';
 
 export async function generateEmbeddings(docId: string) {
   auth().protect(); // Protect this route with Clerk
+  // No one can do this code if not authorize
 
-  // turn a PDF into embeddings [0.0123234, 0.234234, ...]
+  // turn a PDF into a lot of embeddings [0.0123234, 0.234234, ...]
+  // turn a PDF into string of numbers
   await generateEmbeddingsInPineconeVectorStore(docId);
 
   revalidatePath('/dashboard');
